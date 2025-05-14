d2c2c7dfdf7c2b36a1ee239f819a05c9f2ae4a21
manfred <2586729824@qq.com>
2025-05-14 23:12:15 +0800

    添加背景音乐

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            AudioPlayer bgmPlayer = AudioPlayer.playBgm("文明.wav");
             IdentitySelectFrame identitySelectFrame = new IdentitySelectFrame();
             identitySelectFrame.setVisible(true);
         });
